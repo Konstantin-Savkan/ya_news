f0466608a7f225a41a9e3a5ab84344a98df3620f
Konstantin Savkan <savkan@mail.ru>
2024-06-02 00:55:34 +0300

    4

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+
+from django.test.client import Client
+
+from news.models import News
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+
+@pytest.fixture
+def new(author):
+    new = News.objects.create(
+        title='Заголовок',
+        text='Текст',
+        author=author,
+    )
+    return new
--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,10 +1,25 @@
-from http  import HTTPStatus
+from http import HTTPStatus
 
 from django.urls import reverse
+import pytest
 
 
 def test_home_available_for_all(client):
     url = reverse('news:home')
     response = client.get(url)
-    assert response.status_code == HTTPStatus
+    assert response.status_code == HTTPStatus.OK
 
+@pytest.mark.parametrize(
+    'name',
+    ('news:home', 'users:login', 'users:logout', 'users:sigup')
+)
+def test_pages_available_for_all(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_detail_page(client, new):
+    url = reverse('news:detail', args=(new.pk,))
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
